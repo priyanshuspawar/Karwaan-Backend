31a31bc6a95151c5b21f642022e2859928e4420f
priyanshuspawar <priyansusingh306@gmail.com>
2024-05-22 23:04:38 +0530

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT;
 
 initializeModel();
 
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(fileUpload());
